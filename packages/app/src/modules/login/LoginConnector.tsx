d98d30f68b9032629dd8ca6046a10b3a12ee4ba7
Andrei Vasilev <amlk158@gmail.com>
2018-12-27 21:57:42 +0200

    added react native create listin forms

--- packages/app/src/modules/login/LoginConnector.tsx
+++ packages/app/src/modules/login/LoginConnector.tsx
@@ -1,20 +1,11 @@
 import * as React from "react";
 import { LoginController } from "@abb/controller";
-import { LoginView } from './ui/RegisterView';
-import { SecureStore } from "expo";
-import { SID_KEY } from "../shared/constant";
-
+import { LoginView } from "./ui/LoginView";
 
 export class LoginConnector extends React.PureComponent {
-
-  saveSessionId = (sid: string) => {
-    SecureStore.setItemAsync(SID_KEY, sid)
-  }
-
   render() {
-   
     return (
-      <LoginController onSessionid={this.saveSessionId}>
+      <LoginController>
         {({ submit }) => <LoginView submit={submit} />}
       </LoginController>
     );
--- packages/app/src/routes/index.tsx
+++ packages/app/src/routes/index.tsx
@@ -1,17 +1,22 @@
 import * as React from "react";
 
-import { NativeRouter , Route, Switch } from "react-router-native";
-import { RegisterConnector } from '../modules/register/RegisterConnector';
-import { LoginConnector } from '../modules/login/LoginConnector';
+import { NativeRouter, Route, Switch } from "react-router-native";
+import { RegisterConnector } from "../modules/register/RegisterConnector";
+import { LoginConnector } from "../modules/login/LoginConnector";
+import { CreateListingConnector } from "../modules/listing/create/CreateListingConnector";
 
 const Routes = () => (
-  <NativeRouter initialEntries={["/login"]}>
+  <NativeRouter initialEntries={["/listing/create"]}>
     <Switch>
       <Route exact={true} path="/register" component={RegisterConnector} />
       <Route exact={true} path="/login" component={LoginConnector} />
+      <Route
+        exact={true}
+        path="/listing/create"
+        component={CreateListingConnector}
+      />
     </Switch>
   </NativeRouter>
 );
 
 export default Routes;
-
